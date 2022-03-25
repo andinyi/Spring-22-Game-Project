c085b1aa01a48ae2ee46ffa7618ab094963419b2
Andy <hni8449@bths.edu>
2022-03-25 18:38:00 -0400

    glfw game window works

--- Indev/IndevApp.cpp
+++ Indev/IndevApp.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "IndevApp.h"
 #include <iostream>
+#include "GLFW/glfw3.h"
 
 
 namespace Indev
@@ -8,9 +9,22 @@ namespace Indev
 	void IndevApp::Run()
 	{
 		INDEV_LOG("Currently running.."); 
+
+		if (!glfwInit()) 
+		{
+			//Initialization failed
+			INDEV_LOG("GLFW failed to initialize");
+		}
+
+		GLFWwindow* window;
+
+		window = glfwCreateWindow(800, 600, "game window", NULL, NULL);
+
 		while (true) 
 		{
 			OnUpdate();
+			glfwSwapBuffers(window);
+			glfwPollEvents();
 		}
 	}
 	void IndevApp::OnUpdate()
